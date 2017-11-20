999abb862c03521dacbdd40c92407292eb9cd438
vikasgourishetty <vikasgourishetty@gmail.com>
2017-11-20 18:04:34 -0500

    Added changes upto lecture 42.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {name: 'P1', age: 28},
+      {name: 'P2', age: 29},
+      {name: 'P3', age: 26}
+    ],
+    otherState: 'some other state'
+  }
+
+  switchNameHandler = (newName) =>{
+    console.log("Switch button Clicked");
+    this.setState({persons : [
+                              {name: newName, age: 28},
+                              {name: 'P2', age: 29},
+                              {name: 'P3', age: 27}
+                             ]
+                  });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1> VIKAS APP #1</h1>
-        <p> My first react app </p>
-        <Person/>
-        <Person/>
-        <Person/>
+        <h1> My first react App</h1>
+        <button onClick={this.switchNameHandler.bind(this,'P1')} > Switch </button>
+        <Person name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} 
+                 age={this.state.persons[1].age}
+                 click={this.switchNameHandler.bind(this,'Person 1')}> Text Between Opening and closing tag </Person>
+        <Person name={this.state.persons[2].name} 
+                 age={this.state.persons[2].age}/>
       </div>
     );
 
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'VIKAS REACT APP #1'));
-
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const person = () => {
-    return <p> Person Block</p>;
-}
+const person = (props) => {
+return (
+    <div>
+        <p onClick={props.click}> Person Block Person Name : {props.name} and Age : {props.age} !! </p>
+        <p>{props.children}</p>
+    </div>
+    );
+};
 
 export default person;
